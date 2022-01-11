ef05f66af3695599a8c193d27bcb36d0eb3ccf52
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-01-11 22:55:20 +0530

    [neeta mourya] added in getCheapestHotelUC2 branch

--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationSystem.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.*;
 
 public class HotelReservationSystem {
 
@@ -23,4 +24,12 @@ public class HotelReservationSystem {
 		hotelMap.put(hotelType, rate);
 	}
 
-}
+
+	    public static void getCheaper(List<Hotel> hotelList, int dateCount) {
+
+	     Hotel minRateHotel=hotelList.stream().min(Comparator.comparing(Hotel::getRate)).get();
+	        System.out.println("Cheapest hotel :");
+	        System.out.println("Hotel name "+minRateHotel.getHotelName()+", Total Rates : $"+minRateHotel.getRate()*dateCount);
+
+	    }
+	}
